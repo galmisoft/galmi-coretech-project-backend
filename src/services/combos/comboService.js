3c6a1446d86b12427b0f9b11976c0062a1d284a1
Gneric <obiondi99@gmail.com>
2023-07-26 10:30:08 -0500

    endpoints faltantes para admin

---
+++ src/services/combos/comboService.js
@@ -0,0 +1,68 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export class ComboService {
+    static async listProbes() {
+        // id - 
+    }
+    static async listTerrainTypes() {
+        // id - terrainName
+    }
+    static async listPositions() {
+        // id - name
+    }
+    static async listCompanies() {
+        // id - visible_name
+    }
+    static async listUsersEmails() {
+        // id - email
+    }
+    static async listModules() {
+        // id - name
+    }
+    static async listCountries() {
+        // id - name
+    }
+    static async listClients() {
+        // id - comercial_name
+    }
+    static async listProject() {
+        // id - name
+    }
+    static async listTeam() {
+        // id - name
+    }
+    static async listUsers() {
+        // id - name
+    }
+    static async listProductTypes() {
+        // 
+    }
+    static async listMeassures() {
+
+    }
+    static async listProducts() {
+
+    }
+    static async listLines() {
+
+    }
+    static async listClients() {
+
+    }
+    static async listProject() {
+
+    }
+    static async listProductSerialNumber() {
+
+    }
+    static async listActivityTypes() {
+
+    }
+    static async listDocumentTypes() {
+
+    }
+    static async listMailTypes() {
+
+    }
+}
